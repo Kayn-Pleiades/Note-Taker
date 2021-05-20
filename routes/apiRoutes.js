10e4139db714e97a92bb39c3fb9b6a99e986dffd
unknown <kayn.pleiades@gmail.com>
2021-05-20 19:06:41 -0400

    put the file back where it came from

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,14 +4,14 @@ const fs = require('fs'); // Allows interaction with file system
 // Routes
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        let pastNotes = JSON.parse(fs.readFileSync('./public/db/db.json'));
+        let pastNotes = JSON.parse(fs.readFileSync('../db/db.json'));
         res.send(pastNotes);
     });
 
     app.post('/api/notes', (req, res) => {
-        let newNote = JSON.parse(fs.readFileSync('./public/db/db.json'));
+        let newNote = JSON.parse(fs.readFileSync('../db/db.json'));
         newNote.push(req.body);
-        fs.writeFileSync('./public/db/db.json', JSON.stringify(newNote), (error) => error ? console.log(error) : console.log("It worked!!"));
+        fs.writeFileSync('../db/db.json', JSON.stringify(newNote), (error) => error ? console.log(error) : console.log("It worked!!"));
         res.send(newNote);
     });
 };
