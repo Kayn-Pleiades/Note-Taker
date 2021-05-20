841a914559918ff95921f8eeff9dc129de8ffeb3
unknown <kayn.pleiades@gmail.com>
2021-05-20 15:18:57 -0400

    server connections to routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Needed Consts
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // Tells node that we are creating an "express" server
 const app = express();
@@ -10,6 +12,10 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set up routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // Listen if our server has started
 app.listen(PORT, () => {
