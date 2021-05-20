be0120c8cae02ba66d67502dca4f60ecd84226b9
unknown <kayn.pleiades@gmail.com>
2021-05-20 15:43:28 -0400

    set up html routes and organized files

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+// Required Consts
+const path = require("path");
+
+// Routes
+module.exports = (app) => {
+    // To notes
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+    // Default
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+};
