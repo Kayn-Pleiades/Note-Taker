7af89c466bd765c47378a1d88bf351192dd081b6
unknown <kayn.pleiades@gmail.com>
2021-05-20 19:10:32 -0400

    set the path to the db as a const so I can make sure it's going to the right place

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,17 @@
 // Required Consts
 const fs = require('fs'); // Allows interaction with file system
-
+const db = require("../db/db.json")
 // Routes
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        let pastNotes = JSON.parse(fs.readFileSync('../db/db.json'));
+        let pastNotes = JSON.parse(fs.readFileSync(db));
         res.send(pastNotes);
     });
 
     app.post('/api/notes', (req, res) => {
-        let newNote = JSON.parse(fs.readFileSync('../db/db.json'));
+        let newNote = JSON.parse(fs.readFileSync(db));
         newNote.push(req.body);
-        fs.writeFileSync('../db/db.json', JSON.stringify(newNote), (error) => error ? console.log(error) : console.log("It worked!!"));
+        fs.writeFileSync(db, JSON.stringify(newNote), (error) => error ? console.log(error) : console.log("It worked!!"));
         res.send(newNote);
     });
 };
