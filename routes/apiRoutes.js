fbc24f307b6c57299898f82f15cb0e8f29c38d92
unknown <kayn.pleiades@gmail.com>
2021-05-20 17:39:33 -0400

    save notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,18 @@
 // Required Consts
+const { RSA_NO_PADDING } = require('constants');
 const fs = require('fs'); // Allows interaction with file system
 
 // Routes
 module.exports = (app) => {
     app.get('/notes', (req, res) => {
-        let pastNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+        let pastNotes = JSON.parse(fs.readFileSync('../db/db.json'));
         res.send(pastNotes);
     });
 
-    app.post('/notes', (req, res) => {});
+    app.post('/notes', (req, res) => {
+        let newNote = JSON.parse(fs.readFileSync('../db/db.json'));
+        newNote.push(req.body);
+        fs.writeFileSync('../db/db.json', JSON.stringify(newNote));
+        res.send(newNote);
+    });
 };
