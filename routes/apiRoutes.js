cd10a5c960ac2f40fc5463eb145bb799a988f644
unknown <kayn.pleiades@gmail.com>
2021-05-20 18:48:04 -0400

    I think the issue is that it can't find the json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,14 +4,14 @@ const fs = require('fs'); // Allows interaction with file system
 // Routes
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        let pastNotes = JSON.parse(fs.readFileSync('../db/db.json'));
+        let pastNotes = JSON.parse(fs.readFileSync('db/db.json'));
         res.send(pastNotes);
     });
 
     app.post('/api/notes', (req, res) => {
-        let newNote = JSON.parse(fs.readFileSync('../db/db.json'));
+        let newNote = JSON.parse(fs.readFileSync('db/db.json'));
         newNote.push(req.body);
-        fs.writeFileSync('../db/db.json', JSON.stringify(newNote));
+        fs.writeFileSync('db/db.json', JSON.stringify(newNote));
         res.send(newNote);
     });
 };
